b93d14b3e0fa07aec7ee40a85e7dd6de08b81972
Win <tranvietthang2994@gmail.com>
2025-06-06 16:25:12 +0700

    Update code from lesson 32 to 55: Working with Components, Props and JSX

---
+++ 03-pizza-menu/learn/src/index.js
@@ -0,0 +1,142 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  // const style = {
+  //   backgroundColor: "black",
+  //   color: "red",
+  //   textAlign: "center",
+  // };
+
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      {pizzaData.length > 0 ? (
+        <>
+          <p>Check out our delicious pizzas!</p>
+          <p>
+            Authentic Italian cuisine. Authentic Italian cuisine. Authentic
+            Italian cuisine. Authentic Italian cuisine. Authentic Italian
+            cuisine. Authentic Italian cuisine. Authentic Italian cuisine.
+            Authentic Italian cuisine.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObject={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>Sorry, we are still working on our menu.</p>
+      )}
+    </main>
+  );
+}
+
+function Pizza({ pizzaObject }) {
+  return (
+    <li className={"pizza" + (pizzaObject.soldOut ? " sold-out" : "")}>
+      <img src={pizzaObject.photoName} alt={pizzaObject.name} />
+      <div>
+        <h3>{pizzaObject.name}</h3>
+        <p>{pizzaObject.ingredients}</p>
+        <span>{pizzaObject.price + 10}</span>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? <Order /> : <p>Closed, see you later!</p>}
+    </footer>
+  );
+
+  //  return React.createElement("footer", null, "© 2023 Fast React Pizza Co.");
+}
+
+function Order() {
+  return (
+    <div className="order">
+      <h2>Order your pizza</h2>
+      <button className="btn">Order now!</button>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
